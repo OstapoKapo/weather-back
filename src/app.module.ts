148a8b6c96c4e675d8bb52188240a0aff465d9cb
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-05-13 01:28:08 +0300

    fix deploys bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { WeatherModule } from './weather/weather.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CacheModule } from '@nestjs/cache-manager';
 const uri = process.env.MONGO_URL || 'mongodb+srv://ostapokapo:GuB7a0jF33wzGsyh@weather.l8sjq54.mongodb.net/?retryWrites=true&w=majority&appName=weather';
 
 @Module({
